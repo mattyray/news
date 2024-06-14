aa8608ddc07b1b611d2094fb10038732ac3ed65b
matt <mnraynor90@gmail.com>
2024-06-14 12:00:52 -0400

    added permissions

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LogoutView
@@ -12,6 +14,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('home')
