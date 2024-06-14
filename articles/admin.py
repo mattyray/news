08afc5dda765acf9b2cc9940daecf4cf140bec34
matt <mnraynor90@gmail.com>
2024-06-14 16:43:22 -0400

    added comment model, form, admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = [
-        "title", "author", "date"
+    inlines = [
+        CommentInline,
     ]
+    list_display = ["title", "author", "date"]
+
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
