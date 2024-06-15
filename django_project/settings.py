93cc68eeba1428d09eb68b28c21cdc2b7478b7bc
matt <mnraynor90@gmail.com>
2024-06-15 11:52:07 -0400

    deployment prep

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,14 +1,21 @@
 from pathlib import Path
+#new
+from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = "django-insecure-9jra+d7$y-+493=2trtr-xaf!!)$5bfrio@2oj5w*rx3&%8#%q"
+SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["matts-newspaper-applicatiom.fly.dev", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://matts-newspaper-application.fly.dev"]
 
 
 INSTALLED_APPS = [
@@ -17,6 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    #whitenoise - ordeer matters
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     #3rd party
     "crispy_forms",
@@ -30,6 +39,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    #whitenoise - ordeer matters
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -60,10 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3")
 }
 
 
@@ -93,6 +101,20 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+#new
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    'staticfiles': {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
