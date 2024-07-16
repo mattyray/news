81a48c600e8e2a724994643289f988eba7a47154
matt <mnraynor90@gmail.com>
2024-07-16 19:19:22 -0400

    added link to quiz and quiz view/page/template

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, JsGptQuizView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('jsgptquiz/', JsGptQuizView.as_view(), name='jsgptquiz'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+class JsGptQuizView(TemplateView):
+    template_name = 'jsgptquiz.html'
 
 # Create your views here.
