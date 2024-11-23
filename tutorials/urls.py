9e0ca7aa3f74562a654075e7c35e7869707684a9
Matt Raynor <mattraynor@Matts-MacBook-Pro.local>
2024-11-23 15:41:10 -0500

    Add ChatGPT tutorial generation feature

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("chatgpt/", chatgpt_view, name="chatgpt"),  # URL for the ChatGPT page
+    path('generate/', views.generate_tutorial_content, name='generate_tutorial'),
 ]
