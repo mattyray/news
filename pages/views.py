8001e2d38e76e38f4bf7c3d66c4137d9c07ede40
Matt Raynor <mattraynor@Matts-MacBook-Pro.local>
2025-01-10 17:03:06 -0500

    modified fly.toml ad some of the pages app, still fgetting 400 error

--- pages/views.py
+++ pages/views.py
@@ -12,24 +12,8 @@ class HomePageView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
 
 
-class ObjectFlashcardsView(TemplateView):
-    template_name = "tutorials/object_flashcards.html"
 
 
-class ObjectsTutorialView(TemplateView):
-    template_name = "tutorials/objects_tutorial.html"
-
-
-class ObjectQuizView(TemplateView):
-    template_name = "tutorials/object_quiz.html"
-
-
-class TutorialPageView(TemplateView):
-    template_name = "tutorials/tutorial_list.html"
-
-
-class JsGptQuizView(TemplateView):
-    template_name = "jsgptquiz.html"
 
 
 # Create your views here.
